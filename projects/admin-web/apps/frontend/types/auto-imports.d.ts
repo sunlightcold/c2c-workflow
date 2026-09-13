d353fa4595fff48fd162256a90662bd0d7d8efe8
sunlightcold <840289075@qq.com>
2026-09-13 23:20:43 +0800

    chore(@vben/frontend): refresh generated auto imports

--- projects/admin-web/apps/frontend/types/auto-imports.d.ts
+++ projects/admin-web/apps/frontend/types/auto-imports.d.ts
@@ -7,6 +7,7 @@
 export {}
 declare global {
   const EffectScope: typeof import('vue')['EffectScope']
+  const RESOURCE_CONFIRM_Z_INDEX: typeof import('../src/hooks/useResourceGrid')['RESOURCE_CONFIRM_Z_INDEX']
   const acceptHMRUpdate: typeof import('pinia')['acceptHMRUpdate']
   const asyncComputed: typeof import('@vueuse/core')['asyncComputed']
   const autoResetRef: typeof import('@vueuse/core')['autoResetRef']
